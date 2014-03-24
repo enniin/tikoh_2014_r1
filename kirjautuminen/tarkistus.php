aef231d44e61ead01af09c0f7a2fb3ccd19539c9
Elfslayer <tuuli.kahkonen@uta.fi>
2014-03-24 16:07:07 +0200

    Create tarkistus.php
    Tämä tiedosto pitää includella sisällyttää jokaisen sivun (paitsi kirjautumisen) alkuun ja kutsua metodia tarkasta_rooli().

---
+++ kirjautuminen/tarkistus.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+//Tarkistaa, onko käyttäjä kirjautunut sisään.
+//Jos on, funktio palauttaa käyttäjän roolin,
+//muuten palautetaan false.
+function tarkasta_rooli(){
+
+ if(array_key_exists("kirjautunut",$_SESSION))
+  return $_SESSION["rooli"];
+
+ else
+  return false;
+
+}
+
+?>
