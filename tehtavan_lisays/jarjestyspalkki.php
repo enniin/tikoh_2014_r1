19030e5cae29712a5523f21d69025a5c17ad0157
killinki <killinki@users.noreply.github.com>
2014-04-07 23:40:01 +0300

    Create jarjestyspalkki.php
    Luo tehtävälistaan lomakkeen, jonka avulla listan tehtävät voi laittaa eri kriteerein järjestykseen.

---
+++ tehtavan_lisays/jarjestyspalkki.php
@@ -0,0 +1,10 @@
+<?php
+	echo '<form action="tehtavalista.php" method="post">';
+	echo '<p>Järjestä lista:</p>';
+	echo '<label for="t1"><input type="radio" name="luokitus" id="t1" value="teht_id">teht_id</label>';
+	echo '<label for="t2"><input type="radio" name="luokitus" id="t2" value="tyyppi">tyyppi</label>';
+	echo '<label for="t3"><input type="radio" name="luokitus" id="t3" value="luontipvm">luontipvm</label>';
+
+	echo '<input type="hidden" name="tallenna" value="Jatka" />';
+  echo '<input type="submit" name="tehtavanlisays" value="Tee haku" />';
+?>
