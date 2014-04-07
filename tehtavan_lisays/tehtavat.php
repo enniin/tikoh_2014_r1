3d0f7fc8dd2cb5a4a2e5d878166c4dc2fec97e98
killinki <killinki@users.noreply.github.com>
2014-04-07 23:48:32 +0300

    Create tehtavat.php
    Tulostaa kannan tehtävät näkyville.

---
+++ tehtavan_lisays/tehtavat.php
@@ -0,0 +1,39 @@
+<?php
+	function luoLista($lajittelu) {
+			$hakulause = "SELECT teht_id, tyyppi, kuvaus, luontipvm FROM htsysteemi.tehtava ORDER BY $lajittelu";
+			$tulos = pg_query($hakulause);
+	
+			if (!$tulos) {
+				echo "Tehtäviä ei löytynyt.";
+				exit;
+			}
+			
+			echo "<table>";
+				echo "<tr>";
+					echo"<th>teht_id</th>";
+					echo"<th>tyyppi</th>";
+					echo"<th>kuvaus</th>";
+					echo"<th>luonti_pvm</th>";
+				echo "</tr>";
+
+			while ($rivi = pg_fetch_row($tulos)) {
+				echo "<tr>";
+				echo "<td>$rivi[0]</td>";
+				echo "<td>$rivi[1]</td>";
+				echo "<td>".substr($rivi[2], 0, 20)."</td>";
+				echo "<td>$rivi[3]</td>";
+
+				$teht_id = $rivi[0];
+
+				echo "<td><a href='paivitaTehtavaLomake.php?tId=$teht_id'>Muokkaa</a></td>";
+				echo "</tr>";
+			}
+
+			echo "</table>";
+	}
+
+	if (array_key_exists("luokitus",$_POST))
+		luoLista($_POST['luokitus']);
+	else
+		luoLista('teht_id');
+?>
