7eb9e2b47e356b755b29f86347a1cae394537d3a
Elfslayer <tuuli.kahkonen@uta.fi>
2014-04-09 12:46:07 +0300

    Update uusi_sessio.php

--- sessio/uusi_sessio.php
+++ sessio/uusi_sessio.php
@@ -5,7 +5,7 @@ include '../db_connct.php';
 $yhteys = luo_yhteys();
 pg_query("set search_path to htsysteemi");
 
-//Tehdään uusi ses_id manuaalisesti
+//TehdÃ¤Ã¤n uusi ses_id manuaalisesti
 $ses_id_tulos = pg_query("select max(ses_id) from sessio");
 $ses_id_rivi = pg_fetch_row($ses_id_tulos);
 $ses_id = $ses_id_rivi[0];
@@ -17,11 +17,10 @@ $tl_nimi = $_SESSION["tl_nimi"];
 pg_query("insert into sessio(ses_id, kayt_id, tl_nimi, pvm) values('$ses_id', '$kayt_id', '$tl_nimi', current_date)");
 
 
-//Tallennetaan tehtävälista ja session id sessiomuuttujiin. Teht_nro on tällä hetkellä suoritettavan tehtävän numero, 
-//joka alustetaan nollaksi jotta seuraavaksi voidaan luontevasti kasvattaa yhdellä.
 $_SESSION["tlnimi"] = $tlnimi;
 $_SESSION["ses_id"] = $ses_id;
-$_SESSION["teht_nro"] = 0;
+$_SESSION["teht_nro"] = 1;
+$_SESSION["yritys_nro"] = 0;
 
 pg_close();
 
