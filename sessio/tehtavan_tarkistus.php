096ef183d419e8e2b2eb1a415a34d017fbc60cce
Elfslayer <tuuli.kahkonen@uta.fi>
2014-04-09 12:47:20 +0300

    Update tehtavan_tarkistus.php
    Itselleni muistiin: Mystinen ongelma: ratk_yritys ei mene kantaan joka kerta.

--- sessio/tehtavan_tarkistus.php
+++ sessio/tehtavan_tarkistus.php
@@ -1,30 +1,7 @@
 <?php 
 include '../kirjautuminen/tarkistus.php';
 include '../db_connct.php';
-?>
 
-<!DOCTYPE HTML>
-<html lang="fi">
-
-<head>
-<meta charset = "UTF-8" />
-<title>TikoHT - SQL-harjoituksia kaikille!</title>
-
-<link rel="stylesheet" type="text/css" href="../css/basestyle.css" media="screen">
-<link rel="stylesheet" type="text/css" href="../css/printstyle.css" media="print">
-</head>
-
-<body>
-<div id="main">
-
-<header>
-<h1>SQL-harjoituksia</h1>
-</header>
-
-<!-- - - - Tagin <article> sisään sivun varsinainen HTML-sisältö. - - - -->
-<article>
-<?php	 
-include '../listojen_selausta/ulospalikka.php';
 
 $yhteys = luo_yhteys();
 pg_query("set search_path to htsysteemi");
@@ -40,28 +17,44 @@ $teht_id = $teht_id_rivi[0];
 $alk_aika = $_POST["alk_aika"];
 $vastaus = $_POST["vastaus"];
 
-//Varsinainen tehtävän tarkistus tässä.
+//Varsinainen tehtÃ¤vÃ¤n tarkistus tÃ¤ssÃ¤.
 $esim_vast_tulos = pg_query("select esim_vast from esimerkkivastaus where teht_id = $teht_id");
 $esim_vast_rivi = pg_fetch_row($esim_vast_tulos);
 $esim_vast = $esim_vast_rivi[0];
 pg_query("set search_path to esimerkkikanta");
 $oikea_tulos = pg_query($esim_vast);
 
-while ($rivi = pg_fetch_row($oikea_tulos)) {
- for($i = 0; $i < count($rivi); i++) {
-  echo($rivi[$i]);
+$kayttajan_vast_tulos = pg_query($vastaus);
+$oikein = "false";
+
+if($kayttajan_vast_tulos) {
+ while ($oikea_rivi = pg_fetch_row($oikea_tulos)) {
+  if($oikea_rivi == pg_fetch_row($kayttajan_vast_tulos)) {
+   $oikein = "true";
+  }
  }
 }
 
+//Toimenpiteet tuloksen selvittyä.
+if($oikein == "false") {
+ if($_SESSION["yritys_nro"] >= 2) {
+  $_SESSION["teht_nro"]++;
+  $_SESSION["yritys_nro"] = 0;
+ }
+ else
+  $_SESSION["yritys_nro"]++;
+}
 
+if($oikein == "true") {
+ $_SESSION["teht_nro"]++;
+ $_SESSION["yritys_nro"] = 0;
+}
 
 pg_query("set search_path to htsysteemi");
-pg_query("insert into ratk_yritys(ses_id, teht_id, alk_aika, lop_aika, vastaus, oikein) values($ses_id, $teht_id, '$alk_aika', current_time, '$vastaus', true)");
+pg_query("insert into ratk_yritys(ses_id, teht_id, alk_aika, lop_aika, vastaus, oikein) values($ses_id, $teht_id, '$alk_aika', current_time, '$vastaus', $oikein)");
 
 pg_close();
 
+header('Location: ../sessio/tehtava.php');
+
 ?>
-</article>
-</div>
-</body>
-</html>
