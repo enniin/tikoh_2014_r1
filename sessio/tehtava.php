c92e2132cfd226c0e68124f02fcc042a5ba4a641
Elfslayer <tuuli.kahkonen@uta.fi>
2014-04-09 12:47:51 +0300

    Update tehtava.php

--- sessio/tehtava.php
+++ sessio/tehtava.php
@@ -7,54 +7,69 @@ include '../db_connct.php';
 <html lang="fi">
 
 <head>
-<meta charset = "UTF-8" />
+<meta charset="UTF-8" />
 <title>TikoHT - SQL-harjoituksia kaikille!</title>
 
-<link rel="stylesheet" type="text/css" href="../css/basestyle.css" media="screen">
-<link rel="stylesheet" type="text/css" href="../css/printstyle.css" media="print">
+<link rel="stylesheet" type="text/css" href="../css/basestyle.css"
+	media="screen">
+<link rel="stylesheet" type="text/css" href="../css/printstyle.css"
+	media="print">
+<link rel="stylesheet" type="text/css" href="../css/formstyle.css">
 </head>
 
 <body>
-<div id="main">
+	<div id="main">
 
-<header>
-<h1>SQL-harjoituksia</h1>
-</header>
+		<header>
+			<h1>SQL-harjoituksia</h1>
+		</header>
 
-<!-- - - - Tagin <article> sisään sivun varsinainen HTML-sisältö. - - - -->
-<article>
-<?php	 
-include '../listojen_selausta/ulospalikka.php';
-$yhteys = luo_yhteys();
+		<!-- - - - Tagin <article> sisÃ¤Ã¤n sivun varsinainen HTML-sisÃ¤ltÃ¶. - - - -->
+		<article>
+			<?php	 
+			include '../listojen_selausta/ulospalikka.php';
+			$yhteys = luo_yhteys();
 
-pg_query("set search_path to htsysteemi");
+			pg_query("set search_path to htsysteemi");
 
-$ses_id = $_SESSION["ses_id"];
-$tl_nimi = $_SESSION["tl_nimi"];
-$teht_nro = $_SESSION["teht_nro"];
-$teht_nro++;
-$_SESSION["teht_nro"] = $teht_nro;
+			$ses_id = $_SESSION["ses_id"];
+			$tl_nimi = $_SESSION["tl_nimi"];
+			$teht_nro = $_SESSION["teht_nro"];
 
-$teht_id_tulos = pg_query("select teht_id from sisaltyy_listaan where tl_nimi = '$tl_nimi' and nro = '$teht_nro'");
-$teht_id_rivi = pg_fetch_row($teht_id_tulos);
-$teht_id = $teht_id_rivi[0];
+			if($_SESSION["yritys_nro"] > 0) {
+			 echo("<p>VÃ¤Ã¤rin! YritÃ¤ uudelleen!</p>");
+			}
 
-$alk_aika_tulos = pg_query("select current_time");
-$alk_aika_rivi = pg_fetch_row($alk_aika_tulos);
-$alk_aika = $alk_aika_rivi[0];
+			$listan_viimeinen_t_tulos = pg_query("select max(nro) from sisaltyy_listaan where tl_nimi = '$tl_nimi'");
+			$listan_viimeinen_t_rivi = pg_fetch_row($listan_viimeinen_t_tulos);
+			$listan_viimeinen_t = $listan_viimeinen_t_rivi[0];
 
-$teht_kuvaus_tulos = pg_query("select kuvaus from tehtava where teht_id = '$teht_id'");
-$teht_kuvaus_rivi = pg_fetch_row($teht_kuvaus_tulos);
-echo("<h1>TehtÃ¤vÃ¤ $teht_nro</h1><p>$teht_kuvaus_rivi[0]<p><form action='tehtavan_tarkistus.php' method='post'>
+			if($teht_nro > $listan_viimeinen_t) {
+			 echo("<p>TÃ¤ssÃ¤ voidaan sitten nÃ¤yttÃ¤Ã¤ session yhteenveto.<br><a class='napp' href='../listojen_selausta/tehtava_listat.php'>Takaisin listaukseen </a></p>");
+			}
+
+			else {
+			 $teht_id_tulos = pg_query("select teht_id from sisaltyy_listaan where tl_nimi = '$tl_nimi' and nro = '$teht_nro'");
+			 $teht_id_rivi = pg_fetch_row($teht_id_tulos);
+			 $teht_id = $teht_id_rivi[0];
+			 $_SESSION["teht_id"] = $teht_id;
+
+			 $alk_aika_tulos = pg_query("select current_time");
+			 $alk_aika_rivi = pg_fetch_row($alk_aika_tulos);
+			 $alk_aika = $alk_aika_rivi[0];
+
+			 $teht_kuvaus_tulos = pg_query("select kuvaus from tehtava where teht_id = '$teht_id'");
+			 $teht_kuvaus_rivi = pg_fetch_row($teht_kuvaus_tulos);
+			 echo("<h1>TehtÃ¤vÃ¤ $teht_nro</h1><p>$teht_kuvaus_rivi[0]<p><form action='tehtavan_tarkistus.php' method='post'>
 <label for='vastaus'>Vastaus:</label><textarea rows='5' cols='30' name='vastaus' id='vastaus'></textarea><br>
 <input type='submit' value='OK'>
 <input type='hidden' name='alk_aika' value='$alk_aika'>
 </form>");
+			}
+			pg_close();
+			?>
 
-pg_close();
-?>
-
-</article>
-</div>
+		</article>
+	</div>
 </body>
 </html>
