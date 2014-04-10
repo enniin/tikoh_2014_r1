4b72fdb1d64192c697618f040b8cb2328dfb1e07
enniin <jenni.s.saaristo@gmail.com>
2014-04-10 16:05:58 +0300

    Uusi tehtävä -linkitys
    Linkitetty tehtävän lisäys alkunäkymään.

--- listojen_selausta/opett_linkit.php
+++ listojen_selausta/opett_linkit.php
@@ -1,7 +1,7 @@
 <?php
 
 	// Linkit lisäyksiin
-	echo '<a class="napp" href=""> Tehtävien muokkaus ja lisäys </a>';
+	echo '<a class="napp" href="../tehtavan_lisays/tehtavalista.php"> Tehtävien muokkaus ja lisäys </a>';
 	echo '<a class="napp" href=""> Uusi tehtäväsarja </a>';
 			
 ?>
