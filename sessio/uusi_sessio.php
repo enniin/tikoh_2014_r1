7f5de859bea41215c05153d78ee51864f97c1161
Elfslayer <tuuli.kahkonen@uta.fi>
2014-04-11 18:10:32 +0300

    Update uusi_sessio.php

--- sessio/uusi_sessio.php
+++ sessio/uusi_sessio.php
@@ -1,11 +1,12 @@
 <?php 
 include '../kirjautuminen/tarkistus.php';
 include '../db_connct.php';
+tarkasta_rooli();
 
 $yhteys = luo_yhteys();
 pg_query("set search_path to htsysteemi");
 
-//TehdÃ¤Ã¤n uusi ses_id manuaalisesti
+//TehdÃƒÂ¤ÃƒÂ¤n uusi ses_id manuaalisesti
 $ses_id_tulos = pg_query("select max(ses_id) from sessio");
 $ses_id_rivi = pg_fetch_row($ses_id_tulos);
 $ses_id = $ses_id_rivi[0];
@@ -16,8 +17,6 @@ $tl_nimi = $_SESSION["tl_nimi"];
 
 pg_query("insert into sessio(ses_id, kayt_id, tl_nimi, pvm) values('$ses_id', '$kayt_id', '$tl_nimi', current_date)");
 
-
-$_SESSION["tlnimi"] = $tlnimi;
 $_SESSION["ses_id"] = $ses_id;
 $_SESSION["teht_nro"] = 1;
 $_SESSION["yritys_nro"] = 0;
