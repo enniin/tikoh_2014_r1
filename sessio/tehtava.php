70935ff9a7b2c735dfd1e86012dfe5995966836e
Elfslayer <tuuli.kahkonen@uta.fi>
2014-04-17 18:40:01 +0300

    Update tehtava.php
    Kannan rakenne käyttäjälle.

--- sessio/tehtava.php
+++ sessio/tehtava.php
@@ -25,7 +25,7 @@ tarkasta_rooli();
 			<h1>SQL-harjoituksia</h1>
 		</header>
 
-		<!-- - - - Tagin <article> sisÃƒÂ¤ÃƒÂ¤n sivun varsinainen HTML-sisÃƒÂ¤ltÃƒÂ¶. - - - -->
+		<!-- - - - Tagin <article> sisään sivun varsinainen HTML-sisältÃƒÆ’Ã‚Â¶. - - - -->
 		<article>
 			<?php	 
 			include '../listojen_selausta/ulospalikka.php';
@@ -37,16 +37,35 @@ tarkasta_rooli();
 			$tl_nimi = $_SESSION["tl_nimi"];
 			$teht_nro = $_SESSION["teht_nro"];
 
+			
+			//Jos ollaan uudessa yrityksessä, näytetään ilmoitus.
 			if($_SESSION["yritys_nro"] > 0) {
-			 echo("<p>VÃƒÂ¤ÃƒÂ¤rin! YritÃƒÂ¤ uudelleen!</p>");
+			 echo("<p>V&auml;&auml;rin! Yrit&auml; uudelleen!</p>");
+			}
+			
+			//Kannan rakenne käyttäjän tietoon.
+			$taulut_tulos = pg_query("select table_name from information_schema.tables where table_schema = 'esimerkkikanta'");
+			pg_query("set search_path to esimerkkikanta");
+			
+			$metadata = '';
+			while($taulut_rivi = pg_fetch_row($taulut_tulos)) {
+			 $taulun_nimi = $taulut_rivi[0];
+			 $metadata .= "$taulun_nimi(";
+			 $taulu_tiedot = pg_meta_data($yhteys, 'esimerkkikanta.'.$taulun_nimi);
+			 foreach ($taulu_tiedot as $sarake=>$tiedot) {
+			  $metadata .= "$sarake, ";
+			 }
+			 $metadata = substr_replace($metadata, ')', (strlen($metadata)-2));
+			 $metadata .= "<br>";
 			}
 
+			pg_query("set search_path to htsysteemi");
 			$listan_viimeinen_t_tulos = pg_query("select max(nro) from sisaltyy_listaan where tl_nimi = '$tl_nimi'");
 			$listan_viimeinen_t_rivi = pg_fetch_row($listan_viimeinen_t_tulos);
 			$listan_viimeinen_t = $listan_viimeinen_t_rivi[0];
 
 			if($teht_nro > $listan_viimeinen_t) {
-			 echo("<p>TÃƒÂ¤ssÃƒÂ¤ voidaan sitten nÃƒÂ¤yttÃƒÂ¤ÃƒÂ¤ session yhteenveto.<br><a class='napp' href='../listojen_selausta/tehtava_listat.php'>Takaisin listaukseen </a></p>");
+			 echo("<p>T&auml;ss&auml; voidaan sitten n&auml;ytt&auml;&auml; session yhteenveto.<br><a class='napp' href='../listojen_selausta/tehtava_listat.php'>Takaisin listaukseen </a></p>");
 			}
 
 			else {
@@ -61,7 +80,7 @@ tarkasta_rooli();
 
 			 $teht_kuvaus_tulos = pg_query("select kuvaus from tehtava where teht_id = '$teht_id'");
 			 $teht_kuvaus_rivi = pg_fetch_row($teht_kuvaus_tulos);
-			 echo("<h1>TehtÃƒÂ¤vÃƒÂ¤ $teht_nro</h1><p>$teht_kuvaus_rivi[0]<p><form action='tehtavan_tarkistus.php' method='post'>
+			 echo("<h1>Teht&auml;v&auml; $teht_nro</h1><p>$metadata</p><p>$teht_kuvaus_rivi[0]<p><form action='tehtavan_tarkistus.php' method='post'>
 <label for='vastaus'>Vastaus:</label><textarea rows='5' cols='30' name='vastaus' id='vastaus'></textarea><br>
 <input type='submit' value='OK'>
 <input type='hidden' name='alk_aika' value='$alk_aika'>
