bb467fafe94139ce896caaab650e03b617dfa7b0
Elfslayer <tuuli.kahkonen@uta.fi>
2014-04-18 15:23:46 +0300

    Create vastaus.php

---
+++ sessio/vastaus.php
@@ -0,0 +1,67 @@
+<?php
+include '../kirjautuminen/tarkistus.php';
+include '../db_connct.php';
+tarkasta_rooli();
+?>
+
+<!DOCTYPE HTML>
+<html lang="fi">
+
+<head>
+<meta charset="UTF-8" />
+<title>TikoHT - SQL-harjoituksia kaikille!</title>
+
+<link rel="stylesheet" type="text/css" href="../css/basestyle.css"
+	media="screen">
+<link rel="stylesheet" type="text/css" href="../css/printstyle.css"
+	media="print">
+<link rel="stylesheet" type="text/css" href="../css/formstyle.css">
+</head>
+
+<body>
+	<div id="main">
+
+		<header>
+			<h1>SQL-harjoituksia</h1>
+		</header>
+
+		<!-- - - - Tagin <article> sisään sivun varsinainen HTML-sisältö. - - - -->
+		<article>
+			<?php	 
+			include '../listojen_selausta/ulospalikka.php';
+
+			if($_SESSION["oikein"] == "true") {
+			 echo("<p>Oikea vastaus!</p>");
+			}
+
+			else {
+			 //Virheilmoitukset kannasta.
+			 if(array_key_exists("virheilm", $_SESSION)) {
+			  $virheilm = $_SESSION["virheilm"];
+			  echo("<p>Kyselyss&auml; virhe:<br>$virheilm</p>");
+			 }
+
+			 //Käyttäjän vastaus ja oikea vastaus.
+			 else {
+			  $kayttajan_vastaus = $_SESSION["kayttajan_vastaus"];
+			  $oikea_vastaus = $_SESSION["oikea_vastaus"];
+			  echo("<p>V&auml;&auml;rin! Oman kyselysi tulos:<br>$kayttajan_vastaus<br>Oikean vastauksen antama tulos:<br>$oikea_vastaus</p>");
+			 }
+			 //Annetaan mahdollisuus nähdä esimerkkivastaukset.
+			 if(array_key_exists("nayta_ratk", $_SESSION) && $_SESSION["nayta_ratk"] == true) {
+             $_SESSION["nayta_ratk"] = false;
+             echo("<p><a href='esimvastaukset.php'>Katso oikea vastaus</a></p>");
+            }
+			}
+
+
+
+			echo("<p><a href='tehtava.php'>Jatka</a></p>");
+			//print_r($_SESSION);
+
+			?>
+
+		</article>
+	</div>
+</body>
+</html>
