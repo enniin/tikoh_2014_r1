8a628fad3f8b46c6e5135f4dd07690ec13c0d4ac
Elfslayer <tuuli.kahkonen@uta.fi>
2014-04-26 14:17:50 +0300

    Update esimvastaukset.php

--- sessio/esimvastaukset.php
+++ sessio/esimvastaukset.php
@@ -25,26 +25,29 @@ tarkasta_rooli();
 			<h1>SQL-harjoituksia</h1>
 		</header>
 
-		<!-- - - - Tagin <article> sisÃ¤Ã¤n sivun varsinainen HTML-sisÃ¤ltÃ¶. - - - -->
+		<!-- - - - Tagin <article> sisÃƒÂ¤ÃƒÂ¤n sivun varsinainen HTML-sisÃƒÂ¤ltÃƒÂ¶. - - - -->
 		<article>
 			<?php	 
 			include '../listojen_selausta/ulospalikka.php';
 			$yhteys = luo_yhteys();
-			
+
 			$teht_id = $_SESSION["teht_id"];
 			pg_query("set search_path to htsysteemi");
 			$ratk_yritykset_tulos = pg_query("select esim_vast, selitys from esimerkkivastaus where teht_id=$teht_id");
 			while($ratk_yritys_rivi = pg_fetch_row($ratk_yritykset_tulos)) {
              echo("<table><tr><td>$ratk_yritys_rivi[0]</td><td>$ratk_yritys_rivi[1]</td></tr>");
             }
-			
-			echo("</table><p><a href='tehtava.php'>Jatka</a></p>");
-			
-			pg_close();
-			
-			?>
-			
-					</article>
-				</div>
-			</body>
-			</html>
+             
+            echo("</table><p><a href='tehtava.php'>Jatka</a></p>");
+             
+            pg_close();
+             
+            ?>
+
+		</article>
+		<footer role="contentinfo">
+			<p>&copy; K&auml;hk&ouml;nen, Saaristo, Sepp&auml;, TiKO 2014</p>
+		</footer>
+	</div>
+</body>
+</html>
