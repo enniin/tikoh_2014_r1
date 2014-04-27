348af57658abb28829691d12d9f106d201e25388
killinki <killinki@users.noreply.github.com>
2014-04-27 17:29:18 +0300

    Lisätty linkki sarjan lisäykseen

--- listojen_selausta/opett_linkit.php
+++ listojen_selausta/opett_linkit.php
@@ -2,6 +2,6 @@
 
 	// Linkit lisäyksiin
 	echo '<a class="napp" href="../tehtavan_lisays/tehtavalista.php"> Tehtävien muokkaus ja lisäys </a>';
-	echo '<a class="napp" href=""> Uusi tehtäväsarja </a>';
+	echo '<a class="napp" href="../t_sarja/lisaaSarjaLomake.php"> Uusi tehtäväsarja </a>';
 			
 ?>
