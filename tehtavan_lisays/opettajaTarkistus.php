d1ec76ee429d2ef84d066d1d4c7795f0110e35ff
killinki <killinki@users.noreply.github.com>
2014-04-27 17:17:22 +0300

    Update opettajaTarkistus.php

--- tehtavan_lisays/opettajaTarkistus.php
+++ tehtavan_lisays/opettajaTarkistus.php
@@ -3,7 +3,9 @@
 
 	// Tarkistaa, että käyttäjällä on ainakin opettajan oikeudet.
 	// Muussa tapauksessa virheilmoitus.
-	if (tarkasta_rooli() == 'opiskelija') {
+	$rooli = tarkasta_rooli();
+	
+	if ($rooli == 'opiskelija') {
 		echo "Sinulla ei ole valtuuksia toivottuun toimenpiteeseen.";
 		echo "<a href = '../listojen_selausta/tehtava_listat.php'>Palaa etusivulle</a>";
 		exit();
