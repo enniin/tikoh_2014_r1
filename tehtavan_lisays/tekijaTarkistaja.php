a7cc9e667d03dfe5db72c83bea020945a0e8ac3c
killinki <killinki@users.noreply.github.com>
2014-04-27 17:20:40 +0300

    Create tekijaTarkistaja.php

---
+++ tehtavan_lisays/tekijaTarkistaja.php
@@ -0,0 +1,14 @@
+<?php
+	$kayt_id = $_SESSION['kirjautunut'];
+	$tId = $_GET['tId'];
+	
+	// Haetaan listan tekijän id.
+	$tekija_haku = "SELECT kayt_id FROM htsysteemi.tehtava WHERE teht_id = '$tId' AND kayt_id = '$kayt_id'";
+	$loytyiko = pg_num_rows(pg_query($tekija_haku));
+
+	if (!$loytyiko) {
+		echo "<p>Ei oikeuksia!";
+		echo '<a href = "../listojen_selausta/tehtava_listat.php" class = "napp">Palaa etusivulle</a></p>';
+		exit();
+	}
+?>
