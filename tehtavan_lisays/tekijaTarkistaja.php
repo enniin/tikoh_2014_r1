43b7fd9d5ad126c6ea15b627a7f5bdcc91debd6a
killinki <killinki@users.noreply.github.com>
2014-04-28 20:30:08 +0300

    Update tekijaTarkistaja.php

--- tehtavan_lisays/tekijaTarkistaja.php
+++ tehtavan_lisays/tekijaTarkistaja.php
@@ -6,7 +6,7 @@
 	$tekija_haku = "SELECT kayt_id FROM htsysteemi.tehtava WHERE teht_id = '$tId' AND kayt_id = '$kayt_id'";
 	$loytyiko = pg_num_rows(pg_query($tekija_haku));
 
-	if (!$loytyiko) {
+	if (!$loytyiko && $_SESSION['rooli'] != 'yllapitaja') {
 		echo "<p>Ei oikeuksia!";
 		echo '<a href = "../listojen_selausta/tehtava_listat.php" class = "napp">Palaa etusivulle</a></p>';
 		exit();
